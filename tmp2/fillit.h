860a23a19338332dc0ea181cab1e25d683398255
amazurie <amazu@live.fr>
2016-11-16 22:35:44 +0100

    so far...

--- tmp2/fillit.h
+++ tmp2/fillit.h
@@ -7,14 +7,26 @@
 
 # define BUFF_SIZE 21
 
+typedef	struct	    t_hash
+{
+    int		    x;
+    int		    y;
+    struct  t_hash  next;
+}		    t_hash;
 
 typedef struct	    s_tetri
 {
-    int		    w;
-} t_tetri;
+    struct t_hash   *hash;
+    char	    letter;
+    struct s_tetri *next;
+}		    t_tetri;
 
-t_tetri	*get_pieces(char *av);
-int	usageerror();
-int	print_error();
+t_tetri		   *get_pieces(char *av);
+int		    tetri_isvalid(char **tetri);
+int		    check_tetri(char *buff);
+t_tetri		    *store_tetri(t_tetri *tetri, char *buff);
+int		    usageerror();
+int		    print_error();
+void		    init(int i, int j, int k, int l);
 
 #endif
--- tmp2/tetri_handle.c
+++ tmp2/tetri_handle.c
@@ -1,27 +1,19 @@
 #include "fillit.h"
 #include "libft/libft.h"
 
-int	tetri_isvalid(char **tetri)
+int	    save_tetri(t_tetri *piece, char **tetri, int xstart, int ystart)
 {
     int	    i;
     int	    j;
-    int	    count;
 
     i = 0;
-    count = 0;
-    while (tetri[i])
+    while (tetri[ystart + i])
     {
 	j = 0;
-	while (tetri[i][j])
+	while (tetri[ystart + i][xstart + j])
 	{
-	    if (tetri[i][j] == '#')
-	    {
-		count++;
-		if (tetri[i + 1][j] != '#'
-			&& tetri[i][j + 1] != '#'
-			&& count < 4)
-		    return (0);
-	    }
+	    if (tetri[ystart + i][xstart + j] == '#')
+	    
 	    j++;
 	}
 	i++;
@@ -29,67 +21,40 @@ int	tetri_isvalid(char **tetri)
     return (1);
 }
 
-int	check_tetri(char *buff)
+int	    locate_tetri(t_tetri *piece, char **tetri)
 {
-	int	len;
-	int	height;
-	int	hash;
-	int	i;
+    t_hash  *hash;
+    int	    i;
+    int	    j;
+    int	    xstart;
+    int	    ystart;
 
-	len = 0;
-	height = 0;
-	hash = 0;
-	i = 0;
-//	printf("%s", buff);
-	while (buff[i])
-	{
-	    if (buff[i] == '#')
-		hash++;
-	    if (buff[i] == '.' || buff[i] == '#')
-		len++;
-	    if (buff[i] == '\n')
-	    {
-		if ((len != 4 && i < 18) || i > 21)
-		    return (0);
-		height++;
-		len = 0;
-	    }
-	    i++;
-	}
-	if (buff[i - 1] == '\n' && len == 0)
-	    height--;
-	if (height != 4 || hash != 4)
-	    return (0);
-//	printf("%s", buff);
-//	printf("%s \n %d %d \n", buff, height, len);
-	return (1);
+    if (!(hash = (t_hash *)malloc(sizeof(hash))))
+	return (0);
+    i = 0;
+    while (tetri[i])
+    {
+	j = 0;
+	if(ft_strchr(tetri[i], '#') && !ystart)
+	    ystart = i;
+	while (tetri[i][j] && tetri[i][j] != '#')
+	    j++;
+	if (j < xstart)
+	    xstart = j;
+    }
+    return (save_tetri(piece, tetri, xstart, ystart));
 }
 
-t_tetri	    *get_pieces(char *av)
+t_tetri	*store_tetri(t_tetri *piece, char *buff)
 {
-	t_tetri		*pieces;
-	char		*buff;
-	unsigned int	nbcheck;
-	unsigned int	i;
-	int		fd;
-	int		ret;
+    t_tetri	*tmp;
 
-	if (!(fd = open(av, O_RDONLY)))
-	    return (NULL);
-	pieces = (t_tetri *)ft_memalloc(sizeof(t_tetri));
-	buff = (char *)ft_memalloc(BUFF_SIZE + 1);
-	i = 0;
-	while ((ret = read(fd, buff, BUFF_SIZE)))
-	{
-	    buff[ret] == 0;
-	    if (ret == 21)
-		nbcheck++;
-	    if (!(check_tetri(buff)) || ret < 20 || i > 26)
-		return (NULL);
-//	    store_tetri(pieces, buff);
-	    i++;
-	}
-	if (i == 0 || nbcheck == i)
-	    return(NULL);
-	return (pieces);
+    if (!(tmp = (t_tetri *)malloc(sizeof(t_tetri))))
+	return (0);
+    tmp = piece;
+    while (tmp->next)
+	tmp = tmp->next;
+   if (!locate_tetri(piece, ft_strsplit(buff, '\n')))
+       return (NULL);
+    return (piece);
 }
