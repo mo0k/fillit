a617a2ed0c661238630410a4bd49e88f3a652be5
mo0ky <mo0ky@l4p-mo0k.local>
2016-11-18 17:41:52 +0100

    header

--- Rendu/includes/fillit.h
+++ Rendu/includes/fillit.h
@@ -2,7 +2,7 @@
 # define FILLIT_H
 
 # include <fcntl.h>
-# include <unistd.h>
+
 # include <stdio.h>
 
 # define BUFF_SIZE 21
@@ -11,13 +11,13 @@ typedef	struct	    t_hash
 {
     int		    x;
     int		    y;
+    char	    letter;
     struct  t_hash  *next;
 }		    t_hash;
 
 typedef struct	    s_tetri
 {
     struct t_hash   *hash;
-    char	    letter;
     struct s_tetri *next;
 }		    t_tetri;
 
@@ -27,9 +27,10 @@ int		    check_tetri(char *buff);
 int		    usageerror();
 int		    print_error();
 //void		    init(int i, int j, int k, int l);
-int		    locate_tetri(t_tetri *piece, char **tetri);
+int		    locate_tetri(t_tetri *piece, char letter, char **tetri);
 t_tetri		    *store_tetri(t_tetri *piece, char *buff);
-t_hash		    *save_tetri(char **tetri, int xstart, int ystart);
+t_hash		    *save_tetri(char **tetri, char letter, int xstart, int ystart);
+int		    place_tetri(char **map, t_hash *hashs, int i, int j);
 char		**generate_map(size_t size);
 char		**ft_reallloc_map(char **map, int add_size);
 void	display_map(char **map);
