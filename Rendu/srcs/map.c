d1ad3f7e6f521072e4f1c5bd1b1adf82aae6667e
mo0ky <mo0ky@l4p-mo0k.local>
2016-11-18 17:54:52 +0100

    function ft_realloc modified

--- Rendu/srcs/map.c
+++ Rendu/srcs/map.c
@@ -28,7 +28,7 @@ static size_t	map_size(char **map)
 	return (i);
 }
 
-char			**ft_reallloc_map(char **map, int add_size)
+char			**ft_reallloc_map(char **map, int new_size)
 {
 	int			size;
 	char		**new_map;
@@ -36,23 +36,23 @@ char			**ft_reallloc_map(char **map, int add_size)
 
 	i = 0;
 	size = map_size(map);
-	if (!(new_map = (char**)ft_memalloc(size + add_size + 1)))
+	if (!(new_map = (char**)ft_memalloc(new_size + 1)))
 		return (NULL);
-	while (i < size + add_size)
+	while (i < new_size)
 	{
-		new_map[i] = (char*)ft_memalloc(size + add_size + 1);
+		new_map[i] = (char*)ft_memalloc(new_size + 1);
 		if (i < size)
 		{
 			ft_memcpy(new_map[i], map[i], size);
-			if (size < size + add_size)
-				ft_memset(new_map[i] + size, '.', add_size);
+			if (size < new_size)
+				ft_memset(new_map[i] + size, '.', new_size - size);
 		}
-		if (i < size + add_size)
-			ft_memset(new_map[i], '.', size + add_size);
+		if (i < new_size)
+			ft_memset(new_map[i], '.', new_size);
 		i++;
 	}
 	new_map[i] = NULL;
-	//new_map[size + add_size - 1][size + add_size - 1] = '*';
+	//new_map[new_size - 1][new_size - 1] = '*';
 	delete_map(map);
 	return (new_map);
 }
